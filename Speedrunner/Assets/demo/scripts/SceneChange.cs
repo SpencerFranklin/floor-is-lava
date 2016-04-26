117f392fe69e7100c6a418d2ed1cd8fc4ccd8a65
Elizabeth Renn <erenn@skillz.com>
2016-04-26 16:02:33 -0400

    Title scene and fonts

---
+++ Speedrunner/Assets/demo/scripts/SceneChange.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour {
+
+	public void LoadScene(string scene) {
+		SceneManager.LoadScene (scene);
+	}
+}
