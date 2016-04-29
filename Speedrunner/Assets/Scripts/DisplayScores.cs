48d88f82b0cce3514a8efff30d997c90a338913f
dfaz3 <dfaz3@ccs.neu.edu>
2016-04-29 03:48:09 -0400

    Scores scores scores
    Scores now display properly in upper right, and there is an end scene to
    be placed after the last level that displays your final scores

---
+++ Speedrunner/Assets/Scripts/DisplayScores.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DisplayScores : MonoBehaviour {
+
+	public Text level1, level2, level3;
+	private GameObject scoreKeeper;
+
+	// Use this for initialization
+	void Start () {
+		scoreKeeper = GameObject.Find ("ScoreKeeper");
+		level1.text = "Level 1: " + scoreKeeper.GetComponent<KeepScore> ().GetLevelScore (0);
+		level2.text = "Level 2: " + scoreKeeper.GetComponent<KeepScore> ().GetLevelScore (1);
+		level3.text = "Level 3: " + scoreKeeper.GetComponent<KeepScore> ().GetLevelScore (2);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape))
+			Application.Quit ();
+	}
+}
